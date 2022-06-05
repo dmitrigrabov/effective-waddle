b5ab6f08be93a5ed37bb827a87a6b593ea8c39c4
Dmitri Grabov <d.grabov@gmail.com>
2022-06-05 13:08:05 +0100

    access videos

--- source/Background/index.ts
+++ source/Background/index.ts
@@ -1,6 +1,50 @@
-import 'emoji-log';
-import {browser} from 'webextension-polyfill-ts';
+import 'emoji-log'
+import { browser, WebRequest } from 'webextension-polyfill-ts'
+
+const listener = (details: WebRequest.OnBeforeRequestDetailsType) => {
+  const detailsPath =
+    /^https:\/\/twitter.com\/i\/api\/graphql\/[0-9a-z_]*\/TweetDetail\?/gi
+
+  if (!detailsPath.test(details.url)) {
+    return
+  }
+
+  console.log(details)
+
+  const filter = browser.webRequest.filterResponseData(details.requestId)
+  const decoder = new TextDecoder('utf-8')
+  const encoder = new TextEncoder()
+
+  filter.ondata = (event) => {
+    const str = decoder.decode(event.data, { stream: true })
+
+    filter.write(encoder.encode(str))
+    filter.disconnect()
+
+    const tweet = JSON.parse(str)
+
+    const media =
+      tweet?.data?.threaded_conversation_with_injections_v2?.instructions?.[0]
+        ?.entries?.[0]?.content?.itemContent?.tweet_results?.result?.legacy
+        ?.extended_entities?.media?.[0]
+
+    if (!media) {
+      return
+    }
+
+    console.log(media?.video_info?.variants)
+  }
+}
 
 browser.runtime.onInstalled.addListener((): void => {
-  console.emoji('🦄', 'extension installed');
-});
+  console.emoji('🦄', 'extension installed')
+})
+
+browser.webRequest.onBeforeRequest.addListener(
+  listener,
+  {
+    urls: ['https://twitter.com/i/api/graphql/*'],
+    types: ['xmlhttprequest']
+  },
+  ['blocking']
+)
--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -1,10 +1,8 @@
-import * as React from 'react';
-import {browser, Tabs} from 'webextension-polyfill-ts';
-
-import './styles.scss';
+import * as React from 'react'
+import { browser, Tabs } from 'webextension-polyfill-ts'
 
 function openWebPage(url: string): Promise<Tabs.Tab> {
-  return browser.tabs.create({url});
+  return browser.tabs.create({ url })
 }
 
 const Popup: React.FC = () => {
@@ -15,7 +13,7 @@ const Popup: React.FC = () => {
         id="options__button"
         type="button"
         onClick={(): Promise<Tabs.Tab> => {
-          return openWebPage('options.html');
+          return openWebPage('options.html')
         }}
       >
         Options Page
@@ -28,7 +26,7 @@ const Popup: React.FC = () => {
               onClick={(): Promise<Tabs.Tab> => {
                 return openWebPage(
                   'https://github.com/abhijithvijayan/web-extension-starter'
-                );
+                )
               }}
             >
               GitHub
@@ -40,7 +38,7 @@ const Popup: React.FC = () => {
               onClick={(): Promise<Tabs.Tab> => {
                 return openWebPage(
                   'https://www.buymeacoffee.com/abhijithvijayan'
-                );
+                )
               }}
             >
               Buy Me A Coffee
@@ -49,7 +47,7 @@ const Popup: React.FC = () => {
         </ul>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Popup;
+export default Popup
